0f8ba977c930696ab76447a6d391bb75caed7fb9
Elazul <lapiselazul@proton.me>
2025-05-10 23:03:47 +0300

    fix: dev environment issue with turbopack
    Because serwist doesn't support turbopack yet, I had to add a disable variable, and create a separate script for DX with turbopack. See https://github.com/serwist/serwist/issues/54 for more.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
+  disable: process.env.NODE_ENV !== "production", // https://github.com/serwist/serwist/issues/54
 });
 
 export default withSerwist({
