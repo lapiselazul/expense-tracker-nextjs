8826b03c509210c72277298ff24784cdf24d5ec5
Elazul <lapiselazul@proton.me>
2025-05-10 18:45:45 +0300

    feat: add PWA offline functionality

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist({
+  // Your Next.js config
+});
