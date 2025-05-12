cb1b71d00e1ced94c69f9b249964a8741548d754
Elazul <lapiselazul@proton.me>
2025-05-12 13:31:45 +0300

    chore: disable serwist for dev

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
-  disable: true, // https://github.com/serwist/serwist/issues/54
+  disable:  process.env.NODE_ENV !== "production", // https://github.com/serwist/serwist/issues/54#issuecomment-2760476227
 });
 
 export default withSerwist({
